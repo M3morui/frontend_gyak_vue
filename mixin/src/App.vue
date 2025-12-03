8bb0ffad69354fd2d9dbd4528b2b0aa3859737ab
M3morui <vivi.santa0320@gmail.com>
2025-12-03 13:48:17 +0100

    Add files via upload

---
+++ mixin/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Counter from './components/Counter.vue'
+</script>
+
+<template>
+  <div id="app" style="padding: 20px; max-width: 800px; margin: 0 auto;">
+    <h1>Vue 3 Composables (Mixin alternatíva)</h1>
+    <p>Frissítsd az oldalt: az értékek megmaradnak localStorage-ból!</p>
+    <Counter />
+  </div>
+</template>
