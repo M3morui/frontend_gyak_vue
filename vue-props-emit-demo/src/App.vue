26924f8e465726a88b2698b0d24d88ecf0f083e5
M3morui <vivi.santa0320@gmail.com>
2025-12-03 13:49:02 +0100

    Add files via upload

---
+++ vue-props-emit-demo/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TaskList from './components/TaskList.vue'
+import { ref } from 'vue'
+</script>
+
+<template>
+  <div id="app" style="padding: 20px;">
+    <h1>Feladatkezelő - Props/Emit Demo</h1>
+    <TaskList />
+  </div>
+</template>
